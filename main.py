5ea5de36a59e74da7d0202d09fa36519f8b60f64
lucascolas <lucas.colas13@gmail.com>
2023-02-27 14:58:06 +0100

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from texasholdem.game.game import TexasHoldEm
 from texasholdem.gui.text_gui import TextGUI
+from texasholdem.agents.basic import random_agent
 
 game = TexasHoldEm(buyin=500, big_blind=5, small_blind=2, max_players=6)
 gui = TextGUI(game=game)
@@ -8,7 +9,10 @@ while game.is_game_running():
     game.start_hand()
 
     while game.is_hand_running():
-        gui.run_step()
+        gui.display_state()
+        gui.wait_until_prompted()
 
-    path = game.export_history('./pgns')     # save history
-    gui.replay_history(path)                 # replay history
+        game.take_action(*random_agent(game))
+        gui.display_action()
+
+    gui.display_win()
