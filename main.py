1cfdf17bd6aa3a1f2d61093062c25efccc3d7096
lucascolas <lucas.colas13@gmail.com>
2023-02-28 10:33:11 +0100

    variable

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from texasholdem.game.game import TexasHoldEm
 from texasholdem.gui.text_gui import TextGUI
 from texasholdem.agents.basic import random_agent
-
-game = TexasHoldEm(buyin=500, big_blind=5, small_blind=2, max_players=6)
+max_players = 2
+game = TexasHoldEm(buyin=500, big_blind=5, small_blind=2, max_players=max_players)
 gui = TextGUI(game=game)
 
 while game.is_game_running():
