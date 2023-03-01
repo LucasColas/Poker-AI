63d87f881b77e393aac451649501e2d7329fe272
lucascolas <lucas.colas13@gmail.com>
2023-03-01 10:25:24 +0100

    variables for the game.

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from texasholdem.game.game import TexasHoldEm
 from texasholdem.gui.text_gui import TextGUI
 from texasholdem.agents.basic import random_agent
-max_players = 2
-game = TexasHoldEm(buyin=500, big_blind=5, small_blind=2, max_players=max_players)
+max_players = 6
+big_blind = 150
+small_blind = big_blind // 2
+game = TexasHoldEm(buyin=500, big_blind=big_blind, small_blind=small_blind, max_players=max_players)
 gui = TextGUI(game=game)
 
 while game.is_game_running():
     game.start_hand()
-
     while game.is_hand_running():
-        gui.display_state()
-        gui.wait_until_prompted()
-
-        game.take_action(*random_agent(game))
-        gui.display_action()
-
-    gui.display_win()
+        action, total = random_agent(game)
+        print(f"Player {game.current_player} {action} {total}")
+        game.take_action(action, total=total)
