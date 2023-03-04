47d5f8da5476d108ca1d900e0a43fd3af9306323
lucascolas <lucas.colas13@gmail.com>
2023-03-04 08:20:45 +0100

    cards are no longer displayed

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ max_players = 6
 big_blind = 150
 small_blind = big_blind // 2
 game = TexasHoldEm(buyin=500, big_blind=big_blind, small_blind=small_blind, max_players=max_players)
-gui = TextGUI(game=game)
+gui = TextGUI(game=game, visible_players=[])
 
 while game.is_game_running():
     game.start_hand()
+    print(game.hands)
 
     while game.is_hand_running():
-        if game.current_player % 2 == 0:
-            game.take_action(*random_agent(game))
+        #if game.current_player % 2 == 0:
+            #game.take_action(*random_agent(game))
 
         gui.run_step()
