d4545611d7ab706aabdc8811d38f4ae373a9184f
lucascolas <lucas.colas13@gmail.com>
2023-03-08 14:49:22 +0100

    correction cards et ajout variables pour le jeu

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from texasholdem.game.game import TexasHoldEm
 from texasholdem.gui.text_gui import TextGUI
 from texasholdem.agents.basic import random_agent, call_agent
 from texasholdem.evaluator.evaluator import *
+from texasholdem.card.deck import Deck
 from agent import agent_naif
 
-max_players = 3
+max_players = 6
 big_blind = 150
 small_blind = big_blind // 2
-game = TexasHoldEm(buyin=1000, big_blind=big_blind, small_blind=small_blind, max_players=max_players)
+buyin = 1000
+game = TexasHoldEm(buyin=buying, big_blind=big_blind, small_blind=small_blind, max_players=max_players)
 gui = TextGUI(game=game, visible_players=[])
 
 while game.is_game_running():
 
-    game.start_hand()    
+    game.start_hand()
     while game.is_hand_running():
-        
-        game.take_action(*agent_naif(game))
+        if game.current_player == 0:
+            game.take_action(*agent_naif(game))
         gui.set_visible_players([game.current_player])
-        cards = [card for card in cards if card not in game.hands[game.current_player]]
-        gui.run_step()       
-        
-        
+        #cards = Deck(num=52)
+        #cards = [card for card in cards if card not in game.hands[game.current_player]]
+        gui.run_step()
