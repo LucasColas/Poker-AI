b692cf8145d1028af4a1ebdf73d11d2207e6218f
lucascolas <lucas.colas13@gmail.com>
2023-03-21 17:57:44 +0100

    affichage d'un seul joueur

--- simu_bots.py
+++ simu_bots.py
@@ -3,14 +3,19 @@ from texasholdem.gui.text_gui import TextGUI
 from texasholdem.agents.basic import random_agent
 from agent import agent_naif
 
-game = TexasHoldEm(buyin=500, big_blind=5, small_blind=2, max_players=6)
-gui = TextGUI(game=game)
+max_players = 6
+big_blind = 150
+small_blind = big_blind // 2
+buyin = 1000
+game = TexasHoldEm(buyin=buyin, big_blind=big_blind, small_blind=small_blind, max_players=max_players)
+gui = TextGUI(game=game, visible_players=[])
 
 while game.is_game_running():
     game.start_hand()
 
     while game.is_hand_running():
         gui.display_state()
+        gui.set_visible_players([game.current_player])
         gui.wait_until_prompted()
 
         game.take_action(*agent_naif(game))
