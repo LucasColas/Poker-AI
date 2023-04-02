ae1d0fae0dcef48513aa23309f6b974a75c3d238
lucascolas <lucas.colas13@gmail.com>
2023-04-02 16:48:37 +0200

    première version agent outs, fonctionne pour flop

--- agents/agent_outs.py
+++ agents/agent_outs.py
@@ -27,43 +27,49 @@ def agent_outs(game: TexasHoldEm):
 
     elements = player_cards + board_cards
 
+    RANK = {
+        "Straight Flush":1,
+        "Four of a Kind":2,
+        "Full House":3,
+        "Flush":4,
+        "Straight":5,
+        "Three of a Kind":6,
+        "Two Pair":7,
+        "Pair":8,
+        "High card":9,
+    }
+
+
     combinaisons = generer_combinaisons(4, elements)
     print("elements : ", elements)
     info_combi = {}
+    best_possible_hand = {9:0}
     for id, combinaison in enumerate(combinaisons):
         info_combi[id] = defaultdict(int)
         for card in other_cards:
             new_hand = combinaison + (card,)
             rank = evaluate(new_hand[:2], new_hand[2:])
-            info_combi[id][rank_to_string(rank)] += 1
+            rank = RANK[rank_to_string(rank)]
+            info_combi[id][rank] += 1
+            if rank <= list(best_possible_hand.keys())[0]:
+                best_possible_hand = {rank:info_combi[id][rank]}
 
+        info_combi[id] = sorted(info_combi[id].items(), key=lambda item: item[0])
+        #print(info_combi[id])
 
+    chance = best_possible_hand[list(best_possible_hand.keys())[0]]*2
+    print("chance : ", chance)
+    pot_odd = cote_en_pourcentage(obtenir_cote(game))
 
-
-    print("info combi", info_combi)
-
-
-
-
-
-
-    current_best_hand = evaluate(player_cards, board_cards)
-    bet_amount = game.player_bet_amount(game.current_player)
-    chips = game.players[game.current_player].chips
-    min_raise = game.value_to_total(game.min_raise(), game.current_player)
-    max_raise = bet_amount + chips
-    total = None
-    rank = evaluate(game.hands[game.current_player],game.board)
-    p_win = get_five_card_rank_percentage(rank)
-    p = random.random()
     if game.players[game.current_player].state == PlayerState.IN:
         #print("flop check")
         action_type = ActionType.CHECK
-    elif (game.players[game.current_player].state == PlayerState.TO_CALL) and (p<p_win) and (max_raise > min_raise) :
+    elif (game.players[game.current_player].state == PlayerState.TO_CALL) and chance > pot_odd :
         #print("call, p =" ,p, "p_win=",p_win)
         action_type = ActionType.CALL
     else:
         #print("fold, p =", p, " p_win=", p_win)
         action_type = ActionType.FOLD
 
-    return action_type, total
+
+    return action_type, None
