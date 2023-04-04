a70ee6a88dee754379c372d9f7f8072e66084bdf
GuillaumeHERMOSO <126562085+GuillaumeHERMOSO@users.noreply.github.com>
2023-04-04 18:45:46 +0200

    Update stat.py
    debug affichage 1
    
    analyse de nouvelle partie a chaque fois
    
    ajout du profit
    ajout de 2 affichage du proft et du profit moyen

--- agents/stat.py
+++ agents/stat.py
@@ -1,4 +1,4 @@
-from texasholdem.game.game import TexasHoldEm
+from texasholdem.game.game import TexasHoldEm, Pot
 from texasholdem.game.action_type import ActionType
 from texasholdem.game.player_state import PlayerState
 from texasholdem.agents.basic import random_agent
@@ -14,19 +14,20 @@ buyin = 1000
 
 
 
-
-#TODO : ajouter profit
-cles = ["nbrCall", "nbrCheck", "nbrRaise", "nbrFold", "nbrWin", "nbrAllin"]
+# Définir les statistiques à suivre
+cles = ["nbrCall", "nbrCheck", "nbrRaise", "nbrFold", "nbrWin", "nbrAllin", "profit"]
 stats = {cle:{i:0 for i in range(max_players)} for cle in cles}
 
+
 n=0
-nmax=100000
+nmax=10000
 seuil=0.8
-Agent = agent_outs()
+agent_out = agent_outs()
 
 # Définir les fonctions des bots
-bots = [random_agent, agent_naif, Agent.choix, agent_Sacha, random_agent, agent_naif]
-bots_noms = ["random_agent", "agent_naif", "agent_outs", "agent_Sacha", "random_agent", "agent_naif"]
+bots = [random_agent, agent_naif, agent_out.choix, agent_Sacha, random_agent, agent_naif]
+#bot_nom = [i.__name__ for i in bots] sauf pour agent_out.choix devient "agent_out"
+bots_noms = ["random_agent", "agent_naif", "agent_out", "agent_Sacha", "random_agent", "agent_naif"]
 # Initialiser le dictionnaire pour stocker les bots de chaque joueur
 joueurs_bots = {}
 joueurs_bots_noms = {}
@@ -39,82 +40,103 @@ for joueur in range(max_players):
 #print(joueurs_bots)
 while(n<nmax):
     game = TexasHoldEm(buyin=buyin, big_blind=big_blind, small_blind=small_blind, max_players=max_players)
-    while game.is_game_running():
-        game.start_hand()
-        n+=1
+    game.start_hand()
+    n+=1
 
-        while game.is_hand_running():
-            # Utiliser le bot sélectionné pour le joueur actuel attention au bot agent_Sacha qui a 2 paramètres
-            Agent.setGame(game)
-            current_bot = joueurs_bots[game.current_player]
-            if current_bot == Agent.choix:
-                action, total = current_bot()
-            elif(current_bot==agent_Sacha):
-                action, total =current_bot(game,seuil)
-            else:
-                action, total = current_bot(game)
-            #print(f"Player {game.current_player} {action} {total}")
+    while game.is_hand_running():
+        # Utiliser le bot sélectionné pour le joueur actuel attention au bot agent_Sacha qui a 2 paramètres
+        agent_out.setGame(game)
+        current_bot = joueurs_bots[game.current_player]
+        if current_bot == agent_out.choix:
+            action, total = current_bot()
+        elif(current_bot==agent_Sacha):
+            action, total =current_bot(game,seuil)
+        else:
+            action, total = current_bot(game)
+        #print(f"Player {game.current_player} {action} {total}")
 
-            # Mettre à jour les statistiques
-            if (action == ActionType.CALL):
-                stats["nbrCall"][game.current_player]+=1
-            elif (action == ActionType.CHECK):
-                stats["nbrCheck"][game.current_player]+=1
-            elif(action == ActionType.RAISE):
-                stats["nbrRaise"][game.current_player]+=1
-            elif(action == ActionType.FOLD):
-                stats["nbrFold"][game.current_player]+=1
-            elif(action == ActionType.ALL_IN):
-                stats["nbrAllin"][game.current_player]+=1
-            game.take_action(action, total=total)
+        # Mettre à jour les statistiques
+        if (action == ActionType.CALL):
+            stats["nbrCall"][game.current_player]+=1
+        elif (action == ActionType.CHECK):
+            stats["nbrCheck"][game.current_player]+=1
+        elif(action == ActionType.RAISE):
+            stats["nbrRaise"][game.current_player]+=1
+        elif(action == ActionType.FOLD):
+            stats["nbrFold"][game.current_player]+=1
+        elif(action == ActionType.ALL_IN):
+            stats["nbrAllin"][game.current_player]+=1
+        game.take_action(action, total=total)
+    #print(game.hand_history.settle,"=?",game.pots[-1].get_total_amount())
+    gagnant=str(game.hand_history.settle)[7]
+    stats["nbrWin"][int(gagnant)]+=1
+    stats["profit"][int(gagnant)]+= game.pots[-1].get_total_amount()
 
-        gagnant=str(game.hand_history.settle)[7]
-        stats["nbrWin"][int(gagnant)]+=1
-
-        # print(game.hand_history.settle),"\n\n\n")
+    # print(game.hand_history.settle),"\n\n\n")
     # afficher le nombre de parties jouées
     print(n, end="\r")
 
+stats_moy={cle:{i:stats[cle][i]/n for i in range(max_players)} for cle in cles}
 # Afficher les statistiques
-print("call:",stats["nbrCall"],"check:",stats["nbrCheck"],"raise:",stats["nbrRaise"],"fold:",stats["nbrFold"],"\n",sep="\n")
-print(stats["nbrWin"],n)
-#print(joueurs_bots)
+#print("call:",stats["nbrCall"],"check:",stats["nbrCheck"],"raise:",stats["nbrRaise"],"fold:",stats["nbrFold"],"profit",stats["profit"],"\n",sep="\n")
+#print(stats["nbrWin"],n)
+#print(stats_moy["profit"],n)
 
 # Créer un diagramme à barres avec les valeurs de victoires
-plt.bar(joueurs_bots_noms.values(), stats["nbrWin"].values())
+plt.bar(stats["nbrWin"].keys(), stats["nbrWin"].values())
 
-# Ajouter des légendes pour chaque barre
+# Ajouter des informations quel agent + nombre de victoires
 for i, v in enumerate(stats["nbrWin"].values()):
-    #plt.text(i, v, str(v), ha='center')
-    # Ajouter des informations supplémentaires
-    plt.annotate(v, xy=(i, v), ha='center', va='bottom')
+    plt.annotate(f"{joueurs_bots_noms[i]} \n {v}", xy=(i, v), ha='center', va='bottom')
 
 # Ajouter un titre et des étiquettes d'axe
 plt.title("Nombre de victoires pour chaque joueur")
 plt.xlabel("Joueur")
 plt.ylabel("Nombre de victoires")
 
-# Afficher le diagramme
-plt.show()
 
 
 #plot de bar en fonction des valeurs de stats
 width = 0.35
-fig = plt.figure()
+fig = plt.figure(2)
 ax = fig.add_axes([0,0,1,1])
-
 ind = [i for i in range(max_players)]
-ax.bar(ind,list(stats["nbrCall"].values()) , width, color='r')
-ax.bar(ind, list(stats["nbrFold"].values()), width,bottom=list(stats["nbrCall"].values()), color='b')
-ax.bar(ind, list(stats["nbrRaise"].values()), width,bottom=[x + y for x, y in zip(list(stats["nbrCall"].values()), list(stats["nbrFold"].values()))], color='g')
-ax.bar(ind, list(stats["nbrCheck"].values()), width,bottom=[x + y + z for x, y, z in zip(list(stats["nbrCall"].values()), list(stats["nbrFold"].values()), list(stats["nbrRaise"].values()))], color='y')
-ax.bar(ind,list(stats["nbrAllin"].values()), width,bottom=[x + y + z + w for x, y, z, w in zip(list(stats["nbrCall"].values()), list(stats["nbrFold"].values()), list(stats["nbrRaise"].values()), list(stats["nbrCheck"].values()))], color='c')
 
-ax.set_xlabel('Joueur')
-ax.set_ylabel('Nombre de fois')
-ax.set_title('Nombre de fois que chaque joueur a fait une action')
-ax.set_xticks(ind, list(joueurs_bots_noms.values()))
-ax.legend(labels=['nbrCall', 'nbrFold', 'nbrRaise', 'nbrCheck','nbrAllin'])
+#afficher les barres
+bottom = [0 for i in range(max_players)]
+for a,b in stats.items():
+    if (a not in ["nbrWin","profit"]):
+        t= ax.bar(ind,list(b.values()) , width, label=a, bottom=bottom)
+        ax.bar_label(t, label_type='center')
+        bottom = [x + y for x, y in zip(bottom, list(b.values()))]
+ax.set_title("Nombre d'actions pour chaque joueur")
+ax.legend()
 
+#plot bar du profit des joueurs
+fig = plt.figure(3)
+plt.bar(stats["profit"].keys(), stats["profit"].values())
+
+for i, v in enumerate(stats["profit"].values()):
+    plt.annotate(f"{joueurs_bots_noms[i]} \n {v}", xy=(i, v), ha='center', va='bottom')
+
+# Ajouter un titre et des étiquettes d'axe
+plt.title("Profit pour chaque joueur")
+plt.xlabel("Joueur")
+plt.ylabel("Profit (en $)")
+
+
+#plot bar du profit des joueurs / nbr de parties gagnées
+fig = plt.figure(4)
+#creation d'un dictionnaire avec des 0 pour les joueurs qui n'ont pas gagné et sinon on divise le profit par le nombre de victoires
+profit_par_victoire = {i:round(stats["profit"][i]/stats["nbrWin"][i],2) if stats["nbrWin"][i]!=0 else 0 for i in range(max_players)}
+plt.bar(profit_par_victoire.keys(),profit_par_victoire.values() )
+
+for i,v in (profit_par_victoire.items()):
+    plt.annotate(f"{joueurs_bots_noms[i]} \n {v}", xy=(i, v), ha='center', va='bottom')
+
+# Ajouter un titre et des étiquettes d'axe
+plt.title("Profit par victoire pour chaque joueur")
+plt.xlabel("Joueur")
+plt.ylabel("Profit par victoire (en $)")
 
 plt.show()
