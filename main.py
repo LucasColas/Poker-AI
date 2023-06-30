0e4719477db5cbd4cac47657cfa5940c7cd51bb6
lucascolas <lucas.colas13@gmail.com>
2023-06-30 18:55:16 +0200

    add menu

--- main.py
+++ main.py
@@ -20,29 +20,59 @@ import time
 def main():
     start = time.time()
     #tournoi_avec_humain()
+    print("Hello World !")
+    choice = 999
+    
+    while choice != 0:
+        print("0 : leave")
+        print("1 : simulation")
+        print("2 : winners of tournaments")
+        print("3 : watch MCTS play")
+        choice = int(input("What do you want to do ? "))
+        if choice == 0:
+            print("Bye !")
+            return 
+        
+        elif choice == 1:
+            print("Simulation")
+            nmax = int(input("How many simulations ? "))
+            buyin = int(input("Buyin ? "))
+            big_blind = int(input("Big blind ? "))
+            
+            nb_players = int(input("How many players ? "))
+            poolrandom = input("Pool random ? (Y/N) ")
+            if poolrandom == "Y":
+                poolrandom = True
+            else:
+                poolrandom = False
 
+            save = input("Save ? (Y/N) ")
+            if save == "Y":
+                save = True
+            else:
+                save = False
+            plot = input("Plot ? (Y/N) ")
+            if plot == "Y":
+                plot = True
+            else:
+                plot = False
+            get_stat_tournoi(nmax=nmax, buyin=buyin, big_blind=big_blind, save=save, path='./res',plot=plot,poolrandom=poolrandom, max_players=nb_players)
+        
+        elif choice == 2:
+            print("Winners of tournaments")
+            plot_gagnant_from_csv(filename = "./data_gagnant.csv" )
 
+        elif choice == 3:
+            print("MCTS")
+            buyin = int(input("Buyin ? "))
+            big_blind = int(input("Big blind ? "))
+            small_blind = int(input("Small blind ? "))
+            nb_players = int(input("How many players ? "))
+            num_MCTS = int(input("num of player MCTS ? "))
+            MainGame(buyin, big_blind, small_blind, nb_players, num_MCTS)
 
-    
-
-    #plot_gagnant_from_csv(filename = "./data_gagnant.csv" )
-    
-    #get_stat_tournoi(nmax = 100, buyin=1000, big_blind=100, save=False, path='./res', plot=True, poolrandom = False, max_players=6, verbose=False)
-
-    
-    
-    
-    buyin = 1000
-    big_blind = 150
-    small_blind = 75
-    nb_players = 6
-    num_MCTS = 3
-    
-    
-    MainGame(buyin, big_blind, small_blind, nb_players, num_MCTS)
-    
     end = time.time()
-    print(f"Temps d'execution : {end-start} secondes")
+    print(f"Time : {end-start} secondes")
     
     
  
