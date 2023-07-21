05b0ca34971d16c876e888a92468b9068329ce45
lucascolas <lucas.colas13@gmail.com>
2023-07-21 17:57:18 +0200

    found architecture

--- PokerPlus/DeepCFR/nn.py
+++ PokerPlus/DeepCFR/nn.py
@@ -1,54 +1,63 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class DeepCFRNetwork(nn.Module):
-    def __init__(self):
-        super(DeepCFRNetwork, self).__init__()
-        self.num_actions = 3
-        self.cards_embedding = nn.Embedding(52, embedding_dim=32)
-        self.rank_embedding = nn.Embedding(13, embedding_dim=8)
-        self.suit_embedding = nn.Embedding(4, embedding_dim=8)
-        
-        self.cards_fc = nn.Sequential(
-            nn.Linear(32 * 4, 128),
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU()
-        )
-        
-        self.bets_fc = nn.Sequential(
-            nn.Linear(12, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
-            nn.ReLU()
-        )
-        
-        self.final_fc = nn.Sequential(
-            nn.Linear(128 + 64, 256),
-            nn.ReLU(),
-            nn.Linear(256, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, self.num_actions),
-            nn.Softmax(dim=1)
-        )
-    
+class CardEmbedding(nn.Module):
+    def __init__(self, dim):
+        super(CardEmbedding, self).__init__()
+        self.rank = nn.Embedding(13, dim)
+        self.suit = nn.Embedding(4, dim)
+        self.card = nn.Embedding(52, dim)
+
+    def forward(self, input):
+        B, num_cards = input.shape
+        x = input.view(-1)
+        valid = x.ge(0).float()  # -1 means 'no card'
+        x = x.clamp(min=0)
+        embs = self.card(x) + self.rank(x // 4) + self.suit(x % 4)
+        embs = embs * valid.unsqueeze(1)  # zero out 'no card' embeddings
+        return embs.view(B, num_cards, -1).sum(1)
+
+class DeepCFRModel(nn.Module):
+    def __init__(self, n_card_types, n_bets, n_actions, dim=256):
+        super(DeepCFRModel, self).__init__()
+        self.card_embeddings = nn.ModuleList([CardEmbedding(dim) for _ in range(n_card_types)])
+        self.card1 = nn.Linear(dim * n_card_types, dim)
+        self.card2 = nn.Linear(dim, dim)
+        self.card3 = nn.Linear(dim, dim)
+        self.bet1 = nn.Linear(n_bets * 2, dim)
+        self.bet2 = nn.Linear(dim, dim)
+        self.comb1 = nn.Linear(2 * dim, dim)
+        self.comb2 = nn.Linear(dim, dim)
+        self.comb3 = nn.Linear(dim, dim)
+        self.action_head = nn.Linear(dim, n_actions)
+
     def forward(self, cards, bets):
-        cards_embedding = self.cards_embedding(cards)
-        rank_embedding = self.rank_embedding(cards % 13)
-        suit_embedding = self.suit_embedding(cards // 13)
-        
-        cards_sum = torch.sum(cards_embedding, dim=1)
-        rank_sum = torch.sum(rank_embedding, dim=1)
-        suit_sum = torch.sum(suit_embedding, dim=1)
-        
-        cards_representation = torch.cat([cards_sum, rank_sum, suit_sum], dim=1)
-        cards_output = self.cards_fc(cards_representation)
-        
-        bets_output = self.bets_fc(bets)
-        
-        combined_features = torch.cat([cards_output, bets_output], dim=1)
-        
-        final_output = self.final_fc(combined_features)
-        return final_output
+        """
+        cards: ((Nx2), (Nx3) [, (Nx1), (Nx1)]) # (hole, board, [turn, river])
+        bets: Nx n_bet_feats
+        """
+        # 1. card branch
+        # embed hole, flop, and optionally turn and river
+        card_embs = []
+        for embedding, card_group in zip(self.card_embeddings, cards):
+            card_embs.append(embedding(card_group))
+        card_embs = torch.cat(card_embs, dim=1)
+        x = F.relu(self.card1(card_embs))
+        x = F.relu(self.card2(x))
+        x = F.relu(self.card3(x))
+
+        # 2. bet branch
+        bet_size = bets.clamp(0, 1e6)
+        bet_occurred = bets.ge(0)
+        bet_feats = torch.cat([bet_size, bet_occurred.float()], dim=1)
+        y = F.relu(self.bet1(bet_feats))
+        y = F.relu(self.bet2(y) + y)
+
+        # 3. combined trunk
+        z = torch.cat([x, y], dim=1)
+        z = F.relu(self.comb1(z))
+        z = F.relu(self.comb2(z) + z)
+        z = F.relu(self.comb3(z) + z)
+        z = F.normalize(z, p=2, dim=1)  # (z - mean) / std
+        return self.action_head(z)
