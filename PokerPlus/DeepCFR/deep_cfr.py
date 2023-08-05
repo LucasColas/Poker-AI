42ab201781eab63217b78a9b748d3d432b9f2a6b
lucascolas <lucas.colas13@gmail.com>
2023-08-05 15:45:21 +0200

    add : first part of deep cfr algorithm

--- PokerPlus/DeepCFR/deep_cfr.py
+++ PokerPlus/DeepCFR/deep_cfr.py
@@ -1,83 +1,42 @@
+
+from copy import deepcopy
+
 import torch
 import torch.optim as optim
-from PokerPlus.DeepCFR.nn import DeepCFRModel
 
+from PokerPlus.DeepCFR.nn import DeepCFRModel
+from PokerPlus.DeepCFR.game_tree import traverse
+
+from texasholdem.game.game import TexasHoldEm
 
 n_card_types = 4
 n_bets = 2
 n_actions = 3
 
 
-def cfr_iteration(model, data):
-    regrets = {player: torch.zeros(len(actions)) for player, actions in data["actions"]}
-    strategy = {
-        player: torch.ones(len(actions)) / len(actions)
-        for player, actions in data["actions"]
-    }
+def deep_cfr(T : int, nb_players : int, K : int, game : TexasHoldEm, n_actions : int, n_card_types : int, n_bets : int):
+    """
+    
+    Main function of the DeepCFR algorithm.
+    Initialize each player’s advantage network V (I, a|θp) with parameters θp so that it returns 0 for all inputs.
+    Initialize reservoir-sampled advantage memories MV,1, MV,2 and strategy memory MΠ.
+    
+    """
+    # Initialize each player’s advantage network V (I, a|θp) with parameters θp so that it returns 0 for all inputs.
+    advantage_net = DeepCFRModel(n_card_types, n_bets, n_actions)
+    advantage_net.zero_grad()
+    # Initialize reservoir-sampled advantage memories MV,1, MV,2 and strategy memory MΠ.
+    advantage_memories = [torch.zeros(1, 1) for _ in range(nb_players)]
+    strategy_memory = torch.zeros(1, 1)
+    # Initialize the optimizer
+    optimizer = optim.Adam(advantage_net.parameters(), lr=0.001)
 
-    for state, action, payoff in data:
-        # Compute counterfactual regret for each action at the information set
-        # Update regrets dictionary with the computed regrets
-        pass
+    for _ in range(T):
+        for _ in range(nb_players):
+            for _ in range(K):
+                # Traverse the game tree
+                traverse(deepcopy(game), advantage_net, advantage_memories, strategy_memory)
 
-    # Update the strategy using regret-matching algorithm
-    for player, actions in data["actions"]:
-        total_regret = max(regrets[player].sum(), 1.0)  # Avoid division by zero
-        strategy[player] = torch.clamp(regrets[player] / total_regret, 0, 1)
-
-    return strategy
-
-
-def train_value_network(model, data):
-    # Train the value network V using the collected data
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    criterion = torch.nn.MSELoss()
-
-    for state, _, payoff in data:
-        # Compute the target value using the actual payoff
-        target_value = payoff
-
-        # Compute the predicted value from the value network
-        predicted_value = model.value_network(state)
-
-        # Compute the loss and backpropagate
-        loss = criterion(predicted_value, target_value)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            
 
 
-def deep_cfr(train_data, iterations):
-    model = DeepCFRModel(n_card_types, n_bets, n_actions)  # Create the Deep CFR model
-    for _ in range(iterations):
-        # Data collection (game tree traversal)
-        data = game_tree_traversal(train_data, model)
-
-        # CFR iteration to compute regrets and update strategy
-        strategy = cfr_iteration(model, data)
-
-        # Train the value network
-        train_value_network(model, data)
-
-        # Update the strategy network in the model
-        model.update_strategy(strategy)
-
-    return model
-
-
-# Define your game tree traversal function (not provided)
-def game_tree_traversal(train_data, model):
-    # Perform game tree traversal and collect data
-    # Your implementation should return a list of (state, action, payoff) tuples
-    collected_data = None
-    return collected_data
-
-
-# Training data for your specific poker game environment
-train_data = ...
-
-# Number of iterations for the Deep CFR algorithm
-iterations = 100
-
-# Call the deep_cfr function to run the Deep CFR algorithm
-trained_model = deep_cfr(train_data, iterations)
