472ccf58bbdf5f1a4813c0b00d96ed41c46d1396
lucascolas <lucas.colas13@gmail.com>
2023-08-08 15:16:00 +0200

    add : training of advantage network

--- PokerPlus/DeepCFR/deep_cfr.py
+++ PokerPlus/DeepCFR/deep_cfr.py
@@ -29,7 +29,7 @@ def deep_cfr(T : int, nb_players : int, K : int, game : TexasHoldEm, n_actions :
     #advantage_net.zero_grad()
     # Initialize reservoir-sampled advantage memories MV,1, MV,2 and strategy memory MΠ.
     advantage_memories = [AdvantageMemory() for _ in range(nb_players)]
-    strategy_memory = [StrategyMemory() for _ in range(nb_players)]
+    strategy_memory = StrategyMemory()
     # Initialize the optimizer
     optimizer = optim.Adam(advantage_net.parameters(), lr=0.001)
 
@@ -37,10 +37,16 @@ def deep_cfr(T : int, nb_players : int, K : int, game : TexasHoldEm, n_actions :
         for p in range(nb_players):
             for _ in range(K):
                 # Traverse the game tree
-                traverse(deepcopy(game), advantage_net[p], advantage_memories[p], strategy_memory[p])
+                traverse(deepcopy(game), advantage_net[p], advantage_memories[p], strategy_memory)
             
             # Train 
             train_advantage_network(advantage_net[p], advantage_memories[p])
+
+    # Train the strategy network
+    strategy_net = DeepCFRModel(n_card_types, n_bets, n_actions)
+    train_strategy_network(strategy_net, strategy_memory)
+
+    return strategy_net
 
 
             
@@ -65,13 +71,46 @@ def train_advantage_network(net, MV):
                 action_probs = net(cards, bets)
                 loss = F.mse_loss(action_probs, regrets_tensor)
                 losses.append(loss)
+                loss.backward()
+                optimizer.step()
+                
             
             batch_loss = sum(losses) / len(losses)
             total_loss += batch_loss
             
             optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            
         
+        avg_loss = total_loss / num_batches
+        print(f"Epoch [{epoch+1}/4000], Avg Loss: {avg_loss:.4f}")
+
+
+def train_strategy_network(net, M_PI):
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+    batch_size = 10000
+
+    for epoch in range(4000):
+        total_loss = 0.0
+        num_batches = len(M_PI) // batch_size
+
+        for _ in range(num_batches):
+            batch = M_PI.sample(batch_size)
+            losses = []
+
+            for infoset_key, _, sigma_t in batch:
+                cards, bets = infoset_key
+                sigma_t_tensor = torch.tensor(sigma_t, dtype=torch.float32)
+
+                action_probs = net(cards, bets)
+                loss = F.mse_loss(action_probs, sigma_t_tensor)
+                losses.append(loss)
+
+            batch_loss = sum(losses) / len(losses)
+            total_loss += batch_loss
+
+            optimizer.zero_grad()
+            batch_loss.backward()
+            optimizer.step()
+
         avg_loss = total_loss / num_batches
         print(f"Epoch [{epoch+1}/4000], Avg Loss: {avg_loss:.4f}")
