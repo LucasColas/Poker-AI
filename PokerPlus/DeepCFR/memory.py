4d478130389c143c5e0fbb3e4acfef3782b80522
lucascolas <lucas.colas13@gmail.com>
2023-08-30 08:00:19 -0400

    add explicit parameters

--- PokerPlus/DeepCFR/memory.py
+++ PokerPlus/DeepCFR/memory.py
@@ -12,8 +12,8 @@ class AdvantageMemory:
     def __init__(self):
         self.data = set()  # List to store (infoset_key, t, regrets) tuples
 
-    def insert(self, infoset_key, t, regrets):
-        self.data.append((infoset_key, t, regrets))
+    def insert(self, infoset_key : tuple, iteration_t : int, regrets : int):
+        self.data.append((infoset_key, iteration_t, regrets))
 
     def get_data(self):
         return self.data
@@ -26,8 +26,8 @@ class StrategyMemory:
     def __init__(self):
         self.data = {}  # Dictionary to store (infoset_key, t, strategy) pairs
 
-    def insert(self, infoset_key, t, strategy):
-        self.data[(infoset_key, t)] = strategy
+    def insert(self, infoset_key : tuple, iteration_t : int, strategy):
+        self.data[(infoset_key, iteration_t)] = strategy
 
-    def get_strategy(self, infoset_key, t):
-        return self.data.get((infoset_key, t), None)
+    def get_strategy(self, infoset_key : tuple, iteration_t : int):
+        return self.data.get((infoset_key, iteration_t), None)
