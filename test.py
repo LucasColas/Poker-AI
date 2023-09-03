99bbbbef09aadd091464239aae9076818b2db966
lucascolas <lucas.colas13@gmail.com>
2023-09-03 18:16:50 -0400

    add test file for DeepCFR

--- test.py
+++ test.py
@@ -1,36 +1,6 @@
+from PokerPlus.DeepCFR.deep_cfr import deep_cfr, save_deep_cfr
 from texasholdem.game.game import TexasHoldEm
-from texasholdem.gui.text_gui import TextGUI
-from PokerPlus.DeepCFR.game_tree import print_card_mapping
-
-game = TexasHoldEm(buyin=500, big_blind=5, small_blind=2, max_players=2)
-gui = TextGUI(game=game)
-
-
-def heads_up():
-    print("Hello World !")
-    choice = 999
-
-    while choice != 0:
-        print("0 - Quit")
-        print("1 - Simulate a head's up game")
-        choice = int(input("What do you want to do ? "))
-
-        if choice == 1:
-            game = TexasHoldEm(buyin=500, big_blind=5, small_blind=2, max_players=2)
-            gui = TextGUI(game=game)
-
-            while game.is_game_running():
-                game.start_hand()
-
-                while game.is_hand_running():
-                    gui.run_step()
-
-            print("end")
-
 
 def main():
-    print_card_mapping()
-
-
-if __name__ == "__main__":
-    main()
+    game = TexasHoldEm(buyin=1500, big_blind=80, small_blind=40, max_players=2)
+    save_deep_cfr("", "DeepCFR", nb_iterations=10000, nb_players=2, nb_game_tree_traversals=200, game=game, n_actions=3, n_card_types=4, n_bets=20)
